3bb715f90258e8cd81c2fc92da51b818fb62e021
Yadira Soto <lunar13heart@hotmail.com>
2019-06-09 12:05:15 -0400

    added js link in html & edited js

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -1,1 +1,16 @@
-// my js will go in here
+// to maker sure the dom is ready before anything else & making sure it shows up on the console
+// great place to start
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+
+
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=ti59k5SAoYM0q2oy34i4oQmX7V8T39Qc";
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+});
