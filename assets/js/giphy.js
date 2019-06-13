6141ea8d422b8d804b199ebc4f70078c62eba288
Yadira Soto <lunar13heart@hotmail.com>
2019-06-12 22:40:58 -0400

    changed 7 giphs to 10 giphs (hw requirement)

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -45,7 +45,7 @@ $(document).ready(function () {
         var kpop = $(this).attr("data-name");
         console.log(kpop)
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            kpop + "&api_key=ti59k5SAoYM0q2oy34i4oQmX7V8T39Qc&limit=7";
+            kpop + "&api_key=ti59k5SAoYM0q2oy34i4oQmX7V8T39Qc&limit=10";
 
         $.ajax({
             url: queryURL,
